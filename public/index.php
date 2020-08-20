0664a14e02c79f11faa2b72a5674f1130fead8a2
Martin Ruud <m.ruud93@gmail.com>
2020-08-20 14:44:23 +0200

    First commit

--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
     <body>
         <h2>Velkommen til Norge.no</h2>
         <?php
-        echo "<p>Hovedstaden i Norge er: ";
-        echo "<strong>Bergen</strong>";
-        echo "<br />";
+        echo "<h1>Dette er ny versjon</h1>";
         ?>
 
         I dag er det den <?php echo date("d.m.Y");?>
